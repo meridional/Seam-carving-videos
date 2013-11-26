a102e4a75891c98f595b6cbe547adf5329d7ebc6
berilerkin <be521@nyu.edu>
2013-11-26 15:54:01 -0500

    Update carve.m
    Made it slightly faster, by reducing the number of operations in the loop and the number of variables.

--- carve.m
+++ carve.m
@@ -1,16 +1,12 @@
-function [ ri ] = carve( fi, seam )
+function [ fi ] = carve( fi, seam )
 %CARVE takes a frame and a seam, returns a new frame
 % fi is a n-by-m matrix, seam should be a n-dim vector, storing the index
 % of the pixel to be removed at each row
 % CAN work with images of different number of channels
 
-n = size(fi, 1);
-m = size(fi, 2);
-
-ri = zeros([n m-1 size(fi,3)]);
+n = size(fi,1);
 for i = 1:n
-  ri(i,1:(seam(i)-1),:) = fi(i,1:(seam(i)-1),:);
-  ri(i,seam(i):(m-1),:) = fi(i, (seam(i)+1):m,:);
+  fi(i,seam(i):end-1,:) = fi(i,seam(i)+1:end,:);
 end
-
+fi(:,end,:)=[];
 end
